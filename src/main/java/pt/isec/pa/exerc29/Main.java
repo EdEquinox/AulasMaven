8f867aa5371a8142ae5eb92a6711f4f990dfa518
EdEquinox <zemarquez97@hotmail.com>
2023-05-09 16:53:00 +0100

    teste

--- src/main/java/pt/isec/pa/exerc29/Main.java
+++ src/main/java/pt/isec/pa/exerc29/Main.java
@@ -5,7 +5,7 @@ import pt.isec.pa.exerc29.ui.gui.MainJFX;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Base Exerc 29");
+        System.out.println("Base Exerc 29olare");
         Application.launch(MainJFX.class,args);
     }
 }
